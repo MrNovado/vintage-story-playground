8552347c734a5eba5a7e5f77f282f73e832acd24
MrNovado <novado@ymail.com>
2020-03-01 00:13:43 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Abstract
+
+- Build using https://github.com/copygirl/howto-example-mod
+- Simply familiarizing myself with the api and core abstractions (as well as .net)
+- Would also be interesting to apply stateless or any other statecharts tool to model mechanics
---
+++ src/PlaygroundMod.cs
@@ -0,0 +1,29 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+[assembly: ModInfo("Playground",
+    Description = "Playground mod",
+    Website = "https://github.com/MrNovado/vintage-story-playground",
+    Authors = new[] { "mrnovado" })]
+
+namespace Playground
+{
+    public class PlaygroundMod : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+
+        }
+    }
+}
